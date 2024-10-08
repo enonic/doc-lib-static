d740764f02496334136c2ee76a92f47180606cf4
Christian Westgaard <comlock@gmail.com>
2024-10-08 10:52:11 +0200

    Some docs

--- src/main/resources/webapp/webapp.ts
+++ src/main/resources/webapp/webapp.ts
@@ -11,21 +11,34 @@ import {
   router.get('{path:.*}', (request) => requestHandler(
     request,
     {
+
+      // Override the defaultCacheControl by passing our own cacheControl function
       cacheControl: ({
           contentType,
           path,
           resource,
       }) => {
+
+        // The "build/resources/main/webapp/files/vite.svg" file has no contentHash in it's filename,
+        // so it should NOT be using the default immutable cacheControl header.
         if (path.startsWith('/vite.svg')) {
           return RESPONSE_CACHE_CONTROL.SAFE;
         }
+
+        // After the custom logic, fallback to using the defaultCacheControl for everything else.
         return defaultCacheControl({
           contentType,
           path,
           resource,
         });
+
       },
+
+      // When a request does not match any resource file,
+      // serve the SPA index.html, and let the React Router handle routing and potential 404.
       notFound: spaNotFoundHandler,
+
+      // This is the location under build/resources/main where the React app is built to.
       root: '/webapp/files',
     }
   ));
