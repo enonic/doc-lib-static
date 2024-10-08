d4b32ef3d809a9febd1b6a18660c4e08cdb21213
Christian Westgaard <comlock@gmail.com>
2024-10-08 13:46:00 +0200

    whitespace

--- src/main/resources/webapp/webapp.ts
+++ src/main/resources/webapp/webapp.ts
@@ -1,47 +1,47 @@
 import {
-    RESPONSE_CACHE_CONTROL,
-    defaultCacheControl,
-    requestHandler,
-    spaNotFoundHandler,
-    // @ts-expect-error not types
-  } from '/lib/enonic/static';
+  RESPONSE_CACHE_CONTROL,
+  defaultCacheControl,
+  requestHandler,
+  spaNotFoundHandler,
   // @ts-expect-error not types
-  import Router from '/lib/router';
-  const router = Router();
-  router.get('{path:.*}', (request) => requestHandler(
-    request,
-    {
+} from '/lib/enonic/static';
+// @ts-expect-error not types
+import Router from '/lib/router';
+const router = Router();
+router.get('{path:.*}', (request) => requestHandler(
+  request,
+  {
 
-      // Override the defaultCacheControl by passing our own cacheControl function
-      cacheControl: ({
-          contentType,
-          path,
-          resource,
-      }) => {
+    // Override the defaultCacheControl by passing our own cacheControl function
+    cacheControl: ({
+        contentType,
+        path,
+        resource,
+    }) => {
 
-        // The "build/resources/main/webapp/files/vite.svg" file has no contentHash in it's
-        // filename, so it should NOT be using the default immutable cacheControl header.
-        if (path.startsWith('/vite.svg')) {
-          return RESPONSE_CACHE_CONTROL.SAFE;
-        }
+      // The "build/resources/main/webapp/files/vite.svg" file has no contentHash in it's
+      // filename, so it should NOT be using the default immutable cacheControl header.
+      if (path.startsWith('/vite.svg')) {
+        return RESPONSE_CACHE_CONTROL.SAFE;
+      }
 
-        // After the custom logic, fallback to using
-        // the defaultCacheControl for everything else.
-        return defaultCacheControl({
-          contentType,
-          path,
-          resource,
-        });
+      // After the custom logic, fallback to using
+      // the defaultCacheControl for everything else.
+      return defaultCacheControl({
+        contentType,
+        path,
+        resource,
+      });
 
-      },
+    },
 
-      // When a request does not match any resource file, serve the SPA index.html,
-      // and let the React Router handle routing and potential 404.
-      notFound: spaNotFoundHandler,
+    // When a request does not match any resource file, serve the SPA index.html,
+    // and let the React Router handle routing and potential 404.
+    notFound: spaNotFoundHandler,
 
-      // This is the location under build/resources/main where the React app is built to.
-      root: '/webapp/files',
-    }
-  ));
+    // This is the location under build/resources/main where the React app is built to.
+    root: '/webapp/files',
+  }
+));
 
-  export const all = (request) => router.dispatch(request);
+export const all = (request) => router.dispatch(request);
